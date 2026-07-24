260f277e5fd450e9cf2078713d3191b7470eb17a
mulyadibagan <mulyadi.2409@gmail.com>
2026-07-24 23:47:06 +0700

    fix: enable click popup for kawasan hutan layer

--- js/pwa.js
+++ js/pwa.js
@@ -28,7 +28,7 @@
 
     window.addEventListener("load", () => {
       navigator.serviceWorker
-        .register("./service-worker.js?v=20260723-layout-hotfix")
+        .register("./service-worker.js?v=20260724-legend-cache-refresh1")
         .then(registration => {
           console.log("Service worker terdaftar:", registration);
           registration.update();
@@ -37,7 +37,7 @@
     });
 
     navigator.serviceWorker.addEventListener("controllerchange", () => {
-      const reloadKey = "yg-sw-layout-hotfix";
+      const reloadKey = "yg-sw-legend-cache-refresh1";
       if (sessionStorage.getItem(reloadKey)) return;
       sessionStorage.setItem(reloadKey, "done");
       window.location.reload();
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yg-geoportal-v4-20260724-riau-admin-v2";
+const CACHE_NAME = "yg-geoportal-v4-20260724-legend-cache-refresh1";
 
 const STATIC_ASSETS = [
   "./",
