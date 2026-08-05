866285541ecb4f55be3cc815482e89950d4ad1fd
mulyadibagan <48111628+mulyadibagan@users.noreply.github.com>
2026-08-05 09:53:17 +0700

    Move environmental monitoring into layer panel

--- js/ui.js
+++ js/ui.js
@@ -41,11 +41,6 @@
     }
   });
 
-  const layerOrderScript = document.createElement("script");
-  layerOrderScript.src = "js/layer-order-v1.js?v=20260720-2";
-  layerOrderScript.async = true;
-  document.head.appendChild(layerOrderScript);
-
   const monitoringFixScript = document.createElement("script");
   monitoringFixScript.src = "js/map-monitoring-fix-v1.js?v=20260720-1";
   monitoringFixScript.async = true;
